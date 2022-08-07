16b193632283222d82d13447cce2d1fed2353ed8
Derek Bronston <dbronston@me.com>
2022-08-07 14:30:39 -0400

    Initial Commit

---
+++ TestCI/TestCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCIApp.swift
+//  TestCI
+//
+//  Created by Derek Bronston on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TestCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
